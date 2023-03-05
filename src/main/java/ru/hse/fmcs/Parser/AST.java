3af6c408a94eb613679db2f51ce64e838cf0c8fd
malodetz <malo.detz@yandex.ru>
2023-03-06 00:41:29 +0300

    AST impl

--- src/main/java/ru/hse/fmcs/Parser/AST.java
+++ src/main/java/ru/hse/fmcs/Parser/AST.java
@@ -1,9 +1,17 @@
 package ru.hse.fmcs.Parser;
 
+import java.util.ArrayList;
+
 public class AST {
-  private class ASTNode {
+    private class ASTNode {
+        String command;
+        ArrayList<ASTNode> arguments;
 
-  }
+        public ASTNode(String command, ArrayList<ASTNode> arguments) {
+            this.command = command;
+            this.arguments = arguments;
+        }
+    }
 
-  
+    ASTNode root;
 }
