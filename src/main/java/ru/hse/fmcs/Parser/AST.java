25b2411a9350352634076b83e4decfaece118d08
malodetz <malo.detz@yandex.ru>
2023-03-06 01:16:06 +0300

    AST full impl

--- src/main/java/ru/hse/fmcs/Parser/AST.java
+++ src/main/java/ru/hse/fmcs/Parser/AST.java
@@ -2,16 +2,49 @@ package ru.hse.fmcs.Parser;
 
 import java.util.ArrayList;
 
-public class AST {
-    private class ASTNode {
-        String command;
-        ArrayList<ASTNode> arguments;
+abstract class ASTNode {
+}
 
-        public ASTNode(String command, ArrayList<ASTNode> arguments) {
-            this.command = command;
-            this.arguments = arguments;
-        }
+class Command extends ASTNode {
+    String name;
+    ArrayList<ASTNode> arguments;
+
+    public Command(String name, ArrayList<ASTNode> arguments) {
+        this.name = name;
+        this.arguments = arguments;
     }
+}
 
+class Argument extends ASTNode {
+    String value;
+
+    public Argument(String value) {
+        this.value = value;
+    }
+}
+
+class Assignment extends ASTNode {
+    String variable, value;
+
+    public Assignment(String variable, String value) {
+        this.variable = variable;
+        this.value = value;
+    }
+}
+
+class Pipe extends ASTNode {
+    ASTNode left, right;
+
+    public Pipe(ASTNode left, ASTNode right) {
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class AST {
     ASTNode root;
+
+    public AST(ASTNode root) {
+        this.root = root;
+    }
 }
